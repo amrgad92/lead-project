223c7a755bf7fffd8a4bc5594f9368986ad5183d
amrgad92 <amrgad608@gmail.com>
2021-02-22 18:37:29 +0200

    update 22-2-2021

--- js/dist/lead.js
+++ js/dist/lead.js
@@ -293,24 +293,19 @@ $(document).ready(function () {
 
 
 
-        try{
          // SmartWizard initialize
-        $('#smartwizard').smartWizard({
-            // theme: 'dots',
-            // transitionEffect: 'slide',
-            // transitionSpeed: '400',
-            toolbarSettings: {
-                toolbarPosition: 'bottom', // none, top, bottom, both
-                toolbarButtonPosition: 'right', // left, right, center
-                showNextButton: true, // show/hide a Next button
-                showPreviousButton: true, // show/hide a Previous button
-                toolbarExtraButtons: [] // Extra buttons to show on toolbar, array of jQuery input/buttons elements
-            },
-        });
-        }
-        catch(e){
-            console.log(e);
-        }
+         $('#smartwizard2').smartWizard({
+          // theme: 'dots',
+          // transitionEffect: 'slide',
+          // transitionSpeed: '400',
+          toolbarSettings: {
+              toolbarPosition: 'bottom', // none, top, bottom, both
+              toolbarButtonPosition: 'right', // left, right, center
+              showNextButton: true, // show/hide a Next button
+              showPreviousButton: true, // show/hide a Previous button
+              toolbarExtraButtons: [] // Extra buttons to show on toolbar, array of jQuery input/buttons elements
+          },
+      });
 
         $(function(){
             $('.side-child2').click(function(){
